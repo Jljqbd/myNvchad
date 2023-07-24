af1aa89464352b08b5a7bccf110794e4b10375c0
trh <trh@t-debian.www.trh.net>
2023-07-24 15:23:12 +0800

    feat: alt-right=ctrl-i

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -4,6 +4,7 @@ local M = {}
 M.general = {
   n = {
     ["<A-Left>"] = { "<C-o>", "prev postition", opts = { nowait = true } },
+    ["<A-Right>"] = { "<C-i>", "next postition", opts = { nowait = true } },
   },
 }
 
